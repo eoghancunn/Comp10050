6e172fbce06b0266c20eb097dde908d14f3d1ec7
StallionOH <ormondhugh@gmail.com>
2017-03-03 11:59:57 +0000

    Added slots header file

---
+++ src/slots.h
@@ -0,0 +1,17 @@
+/*
+ * slot.h
+ *
+ *  Created on: 3 Mar 2017
+ *      Author: hghrm
+ */
+
+#ifndef SLOT_H_
+#define SLOT_H_
+
+enum slotType {
+	ground,
+	hill,
+	city
+};
+
+#endif /* SLOT_H_ */
